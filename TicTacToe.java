273127982ac823300e493e04bd454af305805dd8
MatthewKosloski <matthewkosloski@me.com>
2017-04-20 20:13:16 -0500

    added play again prompt

--- TicTacToe.java
+++ TicTacToe.java
@@ -23,65 +23,79 @@ public class TicTacToe {
 	private final String SYMBOL_PROMPT = "Desired game piece (%1$s/%2$s):";
 	private final String TURN_PROMPT = "Want to go first? (%1$s/%2$s):";
 	private final String PIECE_PROMPT = "Place piece on tile (0-%1$d):";
+	private final String ANOTHER_PROMPT = "Play again? (%1$s/%2$s):";
 
 	private Board board;
 	private User user;
 	private Computer computer;
 
-	private String userSymbolInput;
-	private String userIndexInput;
+	private String userSymbolInput, userIndexInput, another = YES + "";
 
 	private char userSymbol;
-	private String userFirstTurn = "";
-	private int userIndex = -1;
+	private String userFirstTurn;
+	private int userIndex;
 
 	public TicTacToe() {
 
-		board = new Board(BOARD_SIZE);
-		user = new User(board, X_PIECE);
-		computer = new Computer(board, O_PIECE);
+		while(another.equalsIgnoreCase(YES + "")) {
 
-		// Ask for the user's desired game piece (X or O)
-		while(!(MyUtils.charEqualsIgnoreCase(userSymbol, X_PIECE) || MyUtils.charEqualsIgnoreCase(userSymbol, O_PIECE))) {
-			askForSymbol();
+			userSymbol = ' ';
+			userFirstTurn = "";
+			userIndex = -1;
 
-			try {
-				userSymbol = userSymbolInput.charAt(0);
-			} catch(StringIndexOutOfBoundsException e) {
-				System.out.println(SYMBOL_PROMPT_EXCEPTION);
+			board = new Board(BOARD_SIZE);
+			user = new User(board, X_PIECE);
+			computer = new Computer(board, O_PIECE);
+
+			// Ask for the user's desired game piece (X or O)
+			while(!(MyUtils.charEqualsIgnoreCase(userSymbol, X_PIECE) || MyUtils.charEqualsIgnoreCase(userSymbol, O_PIECE))) {
+				askForSymbol();
+
+				try {
+					userSymbol = userSymbolInput.charAt(0);
+				} catch(StringIndexOutOfBoundsException e) {
+					System.out.println(SYMBOL_PROMPT_EXCEPTION);
+				}
+
 			}
 
-		}
+			// Set both player's symbols
+			user.setSymbol(userSymbol);
+			computer.setSymbol(MyUtils.charEqualsIgnoreCase(userSymbol, X_PIECE) ? O_PIECE : X_PIECE);
 
-		// Set both player's symbols
-		user.setSymbol(userSymbol);
-		computer.setSymbol(MyUtils.charEqualsIgnoreCase(userSymbol, X_PIECE) ? O_PIECE : X_PIECE);
+			// Ask if the user wants to go first
+			while(!(userFirstTurn.equalsIgnoreCase(YES+"") || userFirstTurn.equalsIgnoreCase(NO+""))) {
+				askForFirstTurn();
+			}
 
-		// Ask if the user wants to go first
-		while(!(userFirstTurn.equalsIgnoreCase(YES+"") || userFirstTurn.equalsIgnoreCase(NO+""))) {
-			askForFirstTurn();
-		}
-
-		// If player is first...
-		if(userFirstTurn.equalsIgnoreCase(YES+"")) {
-			userTurn();
-			computer.computeIndex();
-			printBoard();
-		} else {
-			computer.computeIndex();
-			printBoard();
-		}
-
-		while(board.hasAvailableTiles() && !hasWinner()) {
-			userTurn();
-			if(!hasWinner()) {
+			// If player is first...
+			if(userFirstTurn.equalsIgnoreCase(YES+"")) {
+				userTurn();
 				computer.computeIndex();
+				printBoard();
+			} else {
+				computer.computeIndex();
+				printBoard();
 			}
-			printBoard();
+
+			while(board.hasAvailableTiles() && !hasWinner()) {
+				userTurn();
+				if(!hasWinner()) {
+					computer.computeIndex();
+				}
+				printBoard();
+				// System.out.println(board.getBoardString());
+			}
+
+			printGameResult();
+			askAnother();
 		}
 
-		printGameResult();
+	}
 
+	private void askAnother() {
+		System.out.print(String.format(ANOTHER_PROMPT, YES, NO));
+		another = scan.nextLine();
 	}
 
 	private void askForSymbol() {
